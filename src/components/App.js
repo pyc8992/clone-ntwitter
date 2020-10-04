5d705ce1898b3e81da9805c947c3f5c47502cb92
Jayden <pyc8992dev@gmail.com>
2020-10-04 19:33:21 +0900

    added email login

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import AppRouter from './AppRouter';
 import { authService } from "../fbase";
 
 function App() {
-  console.log();
-
+  const [init, setInit] = useState(false);
   const [ isLoggedIn, setIsLoggedIn ] = useState(authService.currentUser);
 
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    })
+  }, []);
+
   return (
-    <div> 
-      <AppRouter isLoggedIn={isLoggedIn}/>
+    <div>
+      { init ? <AppRouter isLoggedIn={isLoggedIn}/> : "initializing..."} 
       <footer>&copy; {new Date().getFullYear()} Clone Ntwitter</footer>
     </div>
   );
