d8228a34271d260e1384f2bb1489a1d2a658be39
Jayden <pyc8992dev@gmail.com>
2020-11-01 16:01:57 +0900

    Updates

---
+++ precache-manifest.8192125f692fd3f2a82eaae87bebcd87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd12fd9eab0689bcedf6d9b929d74ad",
+    "url": "/clone-ntwitter/index.html"
+  },
+  {
+    "revision": "75838d2a731bcb286893",
+    "url": "/clone-ntwitter/static/css/main.b37a7b5f.chunk.css"
+  },
+  {
+    "revision": "afb8f8755bb688b40123",
+    "url": "/clone-ntwitter/static/js/2.3bd2e47d.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/clone-ntwitter/static/js/2.3bd2e47d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75838d2a731bcb286893",
+    "url": "/clone-ntwitter/static/js/main.7ebe4888.chunk.js"
+  },
+  {
+    "revision": "1cc9ba669b12071b0120",
+    "url": "/clone-ntwitter/static/js/runtime-main.36f857bb.js"
+  }
+]);
